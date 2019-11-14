cbeb1e19c7966d8f12572ecb8b38be372789279b
Sheldon Perkins <csperkins18@gmail.com>
2019-11-13 20:51:27 -0500

    setup bamcust.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "playlistDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllSongs();
+  queryDanceSongs();
+});
+
+function queryAllSongs() {
+  connection.query("SELECT * FROM songs", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+    }
+    console.log("-----------------------------------");
+  });
+}
