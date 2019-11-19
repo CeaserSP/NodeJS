de063e19e7152c73395ae1b08aa0c87be66441e8
Sheldon Perkins <csperkins18@gmail.com>
2019-11-18 23:17:35 -0500

    user interger validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,36 +9,48 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   queryAllProducts();
 });
 
 function queryAllProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
       console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
     }
     console.log("-----------------------------------");
+    userPurchase();
   });
 }
 // Set up user inputs to ask for item_id and stock_quantity
-function userPurchase(){
-  inquirer.prompt([{
+function userPurchase() {
+  inquirer.prompt([
+    {
     name: "item_id",
-    type:"input",
-    message:"Enter item_id please.",
-    validate: validate,
+    type: "input",
+    message: "Enter item_id please.",
+    validate: function (value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return "Please insert integer.";
+    },
     filter: Number
-  },{
-      name: "stock_quantity",
-      type:"input",
-      message:"How many would you like?",
-      validate: validate,
-      filter: Number
+  }, {
+    name: "stock_quantity",
+    type: "input",
+    message: "How many would you like?",
+    validate: function (value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return "Please insert integer.";
+    },
+    filter: Number
   }
-]).then(function(answer){}
+  ])
 }
 // TO DO:Query DB to make sure the user input is less than the stock quanity
